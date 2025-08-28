c29675728aa86bbceef458a77809b753865117e8
MeatPine <metra.cosedin@bk.ru>
2025-08-28 15:39:13 +0300

    Update Решение.cpp

--- Вариант 2/№1/Решение.cpp
+++ Вариант 2/№1/Решение.cpp
@@ -1,3 +1,7 @@
+/*
+8 4
+10 20 30 40 188 189 190 191
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,7 +10,7 @@ using namespace std;
 
 int main()
 {
-    
+
     int n, k;
     cin >> n >> k;
     vector<int> sport(n);
@@ -16,19 +20,19 @@ int main()
 
     sort(begin(sport), end(sport));
 
-    int left = 0, right = k - 1;
+    //int left = 0, right = k - 1;
     int MIN = 2000;
-    for (int i = 0; i < n - k; i++) {
+    for (int i = 0; i <= n - k; i++) {
         //cout << left << " " << right << " " << MIN << endl;
-        if (MIN > sport[right] - sport[left]) {
-            MIN = sport[right] - sport[left];
-            left++;
-            right++;
+        if (MIN > sport[i + k - 1] - sport[i]) {
+            cout << "left " << sport[i] << " right " << sport[i + k - 1] << endl;
+            MIN = sport[i + k - 1] - sport[i];
+            cout << MIN << endl;
         }
+        cout << "i " << i << endl;
     }
 
     cout << MIN;
 
     return 0;
 }
-
