b89a27bff45077a8c57e4ddb1085ed515f1b3542
MeatPine <metra.cosedin@bk.ru>
2025-09-02 16:53:22 +0300

    Create 2.cpp

---
+++ Вариант 1/№2/2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int d;
+    cin >> d;
+    vector<int> w(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> w[i];
+    }
+    long long left = 0;
+    long long right = 2e9;
+    while (left + 1 < right)
+    {
+        long long tonage = (left + right) / 2;
+        long long truck1 = 0;
+        long long truck2 = 0;
+        long long count = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (w[i] > 2 * tonage)
+            {
+                count = d + 1;
+                break;
+            }
+            if (truck1 + w[i] <= tonage)
+            {
+                truck1 += w[i];
+            }
+            else if (truck2 + (w[i] - (tonage - truck1)) <= tonage)
+            {
+                int wt = w[i];
+                wt -= tonage - truck1;
+                truck1 = tonage;
+                truck2 += wt;
+            }
+            else
+            {
+                count++;
+                truck1 = w[i];
+                if (w[i] > tonage)
+                {
+                    truck1 = tonage;
+                    truck2 = w[i] - tonage;
+                }
+                else
+                {
+                    truck2 = 0;
+                }
+            }
+        }
+        
+        count++;
+        
+        if (count > d) {
+            left = tonage;
+        }
+        else {
+            right = tonage;
+        }
+
+    }
+    cout << right;
+
+    return 0;
+}
