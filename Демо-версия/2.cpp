35f9beaa4435e5c159d8e0cd1a60d4aab7d09237
MeatPine <metra.cosedin@bk.ru>
2025-09-03 21:04:14 +0300

    Update 2.cpp

--- Демо-версия/2.cpp
+++ Демо-версия/2.cpp
@@ -3,11 +3,27 @@
 
 using namespace std;
 
+int get_days(int n, int mid, vector<int> w) {
+    int days = 1;
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        if (x + w[i] <= mid) {
+            x += w[i];
+            //cout << i << " " << x << endl;
+        }
+        else {
+            days++;
+            x = w[i];
+            //cout << i << " " << x << endl;
+        }
+    }
+
+    return days;
+}
+
 int main() {
-    int n;
-    cin >> n;
-    int d;
-    cin >> d;
+    int d, n;
+    cin >> d >> n;
     vector<int> w(n);
     for (int i = 0; i < n; ++i)
     {
@@ -27,6 +43,7 @@ int main() {
                 count = d + 1;
                 break;
             }
+            
             if (truck1 + w[i] <= tonage)
             {
                 truck1 += w[i];
@@ -38,17 +55,18 @@ int main() {
             }
         }
         count++;
-        
+
         if (count > d) {
             left = tonage;
         }
         else {
             right = tonage;
         }
-        
-        cout << tonage << " " << truck1 << " " << count << endl;
+
+        //cout << tonage << " " << truck1 << " " << count << endl;
     }
-    cout << right;
+    cout << right << endl;
+    cout << get_days(n, right, w);
 
     return 0;
 }
