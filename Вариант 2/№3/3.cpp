d0b1103f18de0a46981662bc4ac6177a1aa955ec
MeatPine <metra.cosedin@bk.ru>
2025-09-08 21:42:25 +0300

    Create 3.cpp

---
+++ Вариант 2/№3/3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> w(n);
+    for (int i = 0; i < n; i++) {
+        cin >> w[i];
+    }
+    vector<int> k(n);
+    for (int i = 0; i < n; i++) {
+        cin >> k[i];
+    }
+
+    vector<int> dp(n);
+    dp[0] = w[0];
+
+    for (int i = 1; i < n; i++) {
+        //cout << i << endl;
+        int MIN = 100001;
+        for (int j = 0; j < i; j++) {
+            if (k[j] >= i - j) {
+                //cout << MIN << " " << dp[j] << endl;
+                MIN = min(MIN, dp[j] + k[j]*k[j]);
+            }
+        }
+        dp[i] = MIN + w[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << dp[i] << " ";
+    }
+
+    cout << endl;
+
+    cout << dp[n - 1];
+
+    return 0;
+}
