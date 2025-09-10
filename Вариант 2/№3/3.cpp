9967cecae4b86c94f19f4627a8099436e0df0e58
MeatPine <metra.cosedin@bk.ru>
2025-09-10 20:59:24 +0300

    Update 3.cpp

--- Вариант 2/№3/3.cpp
+++ Вариант 2/№3/3.cpp
@@ -16,19 +16,32 @@ int main()
         cin >> k[i];
     }
 
-    vector<int> dp(n);
+    vector<int> dp(n, 100001);
     dp[0] = w[0];
 
+    /*
     for (int i = 1; i < n; i++) {
         //cout << i << endl;
         int MIN = 100001;
         for (int j = 0; j < i; j++) {
             if (k[j] >= i - j) {
                 //cout << MIN << " " << dp[j] << endl;
-                MIN = min(MIN, dp[j] + k[j]*k[j]);
+                MIN = min(MIN, dp[j] + (i - j) * c);
             }
         }
         dp[i] = MIN + w[i];
+    }
+    */
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < min(n, i + k[i] + 1); j++) {
+            cout << "i " << i << " j " << j << endl;
+            int x = dp[i] + (j - i) * k[j] + w[j];
+            cout << "x " << x << endl;
+            if (dp[j] > x) {
+                dp[j] = x;
+            }
+        }
     }
 
     for (int i = 0; i < n; i++) {
