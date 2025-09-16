f790b1374f3f4e4b2cc5f3b5bf8ffe5a45f1f4be
MeatPine <metra.cosedin@bk.ru>
2025-09-16 22:41:51 +0300

    Update 2.cpp

--- Вариант 2/№2/2.cpp
+++ Вариант 2/№2/2.cpp
@@ -3,59 +3,19 @@
 
 using namespace std;
 
-int get_days(int n, int mid, vector<int> w) {
-    int days = 1;
-    int x = 0;
-    int truck1 = 0, truck2 = 0;
-    for (int i = 0; i < n; i++) {
-        if (w[i] > 2 * mid)
-        {
-            return days;
-        }
-
-        if (truck1 + w[i] <= mid)
-        {
-            truck1 += w[i];
-        }
-        else if (truck2 + (w[i] - (mid - truck1)) <= mid)
-        {
-            int wt = w[i];
-            wt -= mid - truck1;
-            truck1 = mid;
-            truck2 += wt;
-        }
-        else
-        {
-            days++;
-            truck1 = w[i];
-            if (w[i] > mid)
-            {
-                truck1 = mid;
-                truck2 = w[i] - mid;
-            }
-            else
-            {
-                truck2 = 0;
-            }
-        }
-
-    }
-
-    return days;
-}
-
 int main() {
     int n;
     cin >> n;
     int d;
     cin >> d;
     vector<int> w(n);
+    int left = 0;
+    int right = 0;
     for (int i = 0; i < n; ++i)
     {
         cin >> w[i];
+        right += w[i];
     }
-    long long left = 0;
-    long long right = 2e9;
     while (left + 1 < right)
     {
         long long tonage = (left + right) / 2;
@@ -106,8 +66,7 @@ int main() {
         }
 
     }
-    cout << right << endl;
-    cout << get_days(n, right, w);
+    cout << right;
 
     return 0;
 }
