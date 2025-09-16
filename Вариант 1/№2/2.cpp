a0077da90406ab24c16e0daefa122db6da27de96
MeatPine <metra.cosedin@bk.ru>
2025-09-16 22:43:46 +0300

    Update 2.cpp

--- Вариант 1/№2/2.cpp
+++ Вариант 1/№2/2.cpp
@@ -3,45 +3,11 @@
 
 using namespace std;
 
-int get_days(int n, int mid, vector<int> w) {
-    int days = 1;
-    int x = 0;
-    int truck1 = 0, truck2 = 0;
-    for (int i = 0; i < n; i++) {
-        if (w[i] > mid)
-        {
-            return 100001;
-        }
-
-        if (truck1 + w[i] <= mid)
-        {
-            truck1 += w[i];
-        }
-        else if (truck2 + (w[i] - (mid - truck1)) <= mid)
-        {
-            int wt = w[i];
-            wt -= mid - truck1;
-            truck1 = mid;
-            truck2 += wt;
-        }
-        else
-        {
-            days++;
-            truck1 = w[i];
-            truck2 = 0;
-        }
-
-    }
-
-    return days;
-}
-
-
 int main() {
     int n;
     cin >> n;
-    int D;
-    cin >> D;
+    int d;
+    cin >> d;
     vector<int> w(n);
     int left = 0;
     int right = 0;
@@ -52,37 +18,44 @@ int main() {
     }
     while (left + 1 < right)
     {
+        long long tonage = (left + right) / 2;
+        long long truck1 = 0;
+        long long truck2 = 0;
+        long long count = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (w[i] > tonage)
+            {
+                count = d + 1;
+                break;
+            }
+            if (truck1 + w[i] <= tonage)
+            {
+                truck1 += w[i];
+            }
+            else if (truck2 + w[i] <= tonage)
+            {
+                
+            }
+            else
+            {
+                count++;
+                truck1 = w[i];
+                truck2 = 0;
+            }
+        }
 
-        int mid = left + (right - left) / 2;
-        //cout << left << " " << right << " " << get_days(n, mid, w) << endl;
-        int d = get_days(n, mid, w);
+        count++;
 
-        if (d < D) {
-            left = mid + 1;
+        if (count > d) {
+            left = tonage;
         }
         else {
-            right = mid - 1;
+            right = tonage;
         }
+
     }
-
-    //cout << endl;
-
-    //cout << left << " " << right << " " << get_days(n, right, w) << endl;
-
     cout << right;
 
     return 0;
 }
-
-
-/*
-5 2
-4 1 3 2 5
-
-5
-*/
-
-/*
-6 3
-3 2 2 4 1 4
-*/
