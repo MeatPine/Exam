34b9091ca6bb8613406f1e9568e14626cbb4fe4a
MeatPine <metra.cosedin@bk.ru>
2025-09-17 22:51:09 +0300

    Update Kraskala.cpp

--- Алгоритмы/Kraskala.cpp
+++ Алгоритмы/Kraskala.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 int find(vector<int>& comp, int x) {
     if (comp[x] != x)
         comp[x] = find(comp, comp[x]);
@@ -6,38 +12,40 @@ int find(vector<int>& comp, int x) {
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector <vector <int> > edges(m, vector<int>(3));
-        for (int i = 0; i < m; ++i)
-            cin >> edges[i][1] >> edges[i][2] >> edges[i][0];
-    sort(edges.begin(), edges.end());
-    vector <int> comp(n);
-    for (int i = 0; i < n; ++i)
+int n, m;
+cin >> n >> m;
+vector <vector <int> > edges(m, vector<int>(3));
+for (int i = 0; i < m; ++i)
+    cin >> edges[i][1] >> edges[i][2] >> edges[i][0];
+sort(edges.begin(), edges.end());
+vector <int> comp(n);
+for (int i = 0; i < n; ++i)
     comp[i] = i;
 
-    int ans = 0;
-    int edges_used = 0;
+int ans = 0;
+int edges_used = 0;
 
 
-    for (auto & edge: edges){
-        int weight = edge[0];
-        int start  = edge[1];
-        int end    = edge[2];
+for (auto & edge: edges)
+{
+    int weight = edge[0];
+    int start  = edge[1];
+    int end    = edge[2];
 
-        int root_start = find(comp, start);
-        int root_end = find(comp, end);
+    int root_start = find(comp, start);
+    int root_end = find(comp, end);
 
-        if (root_start != root_end) {
+    if (root_start != root_end) {
             ans += weight;
             comp[root_end] = root_start; 
             edges_used++;
         }
  }
  
-    if (edges_used != n - 1) {
-        cout << "-1" << endl;
-    } 
-    else {
-        cout << ans << endl;
-    }
+if (edges_used != n - 1) {
+    cout << "-1" << endl;
+} else {
+    cout << ans << endl;
+}
+ return 0;
+}
