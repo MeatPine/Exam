ff32a11a2962117d8d74d2f5be376001db41b4f9
MeatPine <metra.cosedin@bk.ru>
2025-09-17 22:42:07 +0300

    Update Kraskala.cpp

--- Алгоритмы/Kraskala.cpp
+++ Алгоритмы/Kraskala.cpp
@@ -1,25 +1,43 @@
-int n, m;
-cin >> n >> m;
-vector <vector <int> > edges(m, vector<int>(3));
-for (int i = 0; i < m; ++i)
-    cin >> edges[i][1] >> edges[i][2] >> edges[i][0];
-sort(edges.begin(), edges.end());
-vector <int> comp(n);
-for (int i = 0; i < n; ++i)
-    comp[i] = i;
-int ans = 0;
-for (auto & edge: edges)
+int find(vector<int>& comp, int x) {
+    if (comp[x] != x)
+        comp[x] = find(comp, comp[x]);
+    return comp[x];
+}
+
+int main()
 {
-    int weight = edge[0];
-    int start  = edge[1];
-    int end    = edge[2];
-    if (comp[start] != comp[end])
-    {
-        ans += weight;
-        int a = comp[start];
-        int b = comp[end];
-        for (int i = 0; i < n; ++i)
-            if (comp[i] == b)
-                comp[i] = a;
-     }
+    int n, m;
+    cin >> n >> m;
+    vector <vector <int> > edges(m, vector<int>(3));
+        for (int i = 0; i < m; ++i)
+            cin >> edges[i][1] >> edges[i][2] >> edges[i][0];
+    sort(edges.begin(), edges.end());
+    vector <int> comp(n);
+    for (int i = 0; i < n; ++i)
+    comp[i] = i;
+
+    int ans = 0;
+    int edges_used = 0;
+
+
+    for (auto & edge: edges){
+        int weight = edge[0];
+        int start  = edge[1];
+        int end    = edge[2];
+
+        int root_start = find(comp, start);
+        int root_end = find(comp, end);
+
+        if (root_start != root_end) {
+            ans += weight;
+            comp[root_end] = root_start; 
+            edges_used++;
+        }
  }
+ 
+    if (edges_used != n - 1) {
+        cout << "-1" << endl;
+    } 
+    else {
+        cout << ans << endl;
+    }
