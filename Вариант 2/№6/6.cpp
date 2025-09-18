3eb0e608876c5c95b5db95694769a147a62cbd31
MeatPine <metra.cosedin@bk.ru>
2025-09-18 18:57:36 +0300

    Update 6.cpp

--- Вариант 2/№6/6.cpp
+++ Вариант 2/№6/6.cpp
@@ -6,88 +6,114 @@ using namespace std;
 
 const int INF = 2e9;
 
-struct X {
+struct ROAD {
     int v;
     int t;
     int p;
 };
 
-int Deiksrta(int start, vector<vector<X>> graph, int finish, int p) {
-    vector<int> rast(graph.size(), INF);
-    rast[start] = 0;
+vector<int> dijkstra(int start, int n, vector<vector<ROAD>> graph, int p) {
+    vector<int> dist(n, INF);
+    dist[start] = 0;
+    set<pair<int, int>> pq;
+    pq.insert({ 0, start });
 
-    set <pair<int, int>> Q;
-    Q.insert({ 0, start });
+    while (!pq.empty()) {
+        int u = pq.begin()->second;
+        int d = pq.begin()->first;
+        pq.erase(pq.begin());
 
-    while (!Q.empty()) {
-        int u = Q.begin()->second;
-        if (u == finish) {
-            return rast[finish];
-        }
-        Q.erase(Q.begin());
+        if (d > dist[u]) continue;
 
-        for (auto road : graph[u]) {
-            if (road.p <= p) {
-                if (rast[u] + road.t < rast[road.v]) {
-                    Q.erase({ rast[road.v], road.v });
-                    rast[road.v] = rast[u] + road.t;
-                    Q.insert({ rast[road.v], road.v });
+        for (const auto& edge : graph[u]) {
+            int road_p = edge.p;
+            int v = edge.v;
+            int wt = edge.t;
+            if (dist[u] + wt < dist[v]) {
+                if (road_p <= p) {
+                    dist[v] = dist[u] + wt;
+                    pq.insert({ dist[v], v });
                 }
             }
         }
     }
-
-    return -1;
+    return dist;
 }
 
-int main()
-{
+int main() {
 
     int n, m;
     cin >> n >> m;
-
-    vector<vector<X>> graph(m);
+    vector<vector<ROAD>> graph(n);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        int t;
-        cin >> t;
-        int p;
-        cin >> p;
+        int t, p;
+        cin >> t >> p;
 
-        X road;
+        ROAD road;
         road.v = v;
-        road.t = t;
         road.p = p;
-
+        road.t = t;
         graph[u].push_back(road);
         road.v = u;
         graph[v].push_back(road);
     }
+
     int x;
     cin >> x;
-
-    int p = 0, r = 0;
-    int left = 0, right = 100000;
-    while (left + 1 < right) {
-        int middle = (left + right) / 2;
-        int rast = Deiksrta(0, graph, x, middle);
-        if (rast != -1) {
-            right = middle;
-            p = middle;
-            r = rast;
+    /*
+    vector<int> dist = dijkstra(0, n, graph, 1);
+    for (int i = 0; i < dist.size(); i++) {
+        cout << dist[i] << " ";
+    }
+    */
+    vector<int> dist = dijkstra(0, n, graph, INF);
+    if (dist[x] == INF)
+    {
+        cout << -1 << endl;
+        return 0;
+    }
+    int left = 0, right = 100001;
+    int l, k;
+    l = 0;
+    k = INF;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        dist = dijkstra(0, n, graph, mid);
+        cout << left << " " << right << " " << mid << " " << k << endl;
+        if (dist[x] != INF) {
+            right = mid - 1;
+            l = mid;
+            k = dist[x];
         }
         else {
-            left = middle + 1;
-            p = middle;
-            r = rast;
+            left = mid + 1;
+            //l = mid;
+            //k = dist[x];
         }
-        cout << middle << " " << rast << " " << left << " " << right << endl;
-
     }
 
-    cout << p << " " << r;
+    cout << k << " " << l;
 
     return 0;
 }
 
+/*
+5 4
+0 1 2 10
+0 2 2 10
+1 3 2 10
+2 3 1 10
+3
+
+*/
+
+/*
+4 4
+0 1 100 50
+0 2 150 30
+1 3 200 40
+2 3 100 60
+3
+*/
